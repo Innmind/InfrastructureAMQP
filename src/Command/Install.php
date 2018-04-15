bacb64f9bdb5b60992eab99078458301fa74b063
Baptiste Langlade <baptouuuu@gmail.com>
2018-04-15 11:59:37 +0200

    add install command

---
+++ src/Command/Install.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\InfrastructureAMQP\Command;
+
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Server\Control\{
+    Server,
+    Server\Command as ServerCommand,
+    Server\Process\ExitCode,
+};
+use Innmind\Immutable\Stream;
+
+final class Install implements Command
+{
+    private $server;
+    private $actions;
+
+    public function __construct(Server $server)
+    {
+        $this->server = $server;
+        $this->actions = Stream::of(
+            'string',
+            'echo "deb https://dl.bintray.com/rabbitmq/debian stretch main" | tee /etc/apt/sources.list.d/bintray.rabbitmq.list',
+            'wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -',
+            'apt-get update',
+            'apt-get install libsctp1 -y',
+            'wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_20.3-1~debian~stretch_amd64.deb',
+            'dpkg -i esl-erlang_20.3-1~debian~stretch_amd64.deb',
+            'rm esl-erlang_20.3-1~debian~stretch_amd64.deb',
+            'apt-get install rabbitmq-server -y',
+            'rabbitmq-plugins enable rabbitmq_management',
+            'wget http://localhost:15672/cli/rabbitmqadmin',
+            'chmod +x rabbitmqadmin',
+            'mv rabbitmqadmin /usr/local/bin/rabbitmqadmin'
+        );
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        $processes = $this->server->processes();
+        $exitCode = $this->actions->reduce(
+            new ExitCode(0),
+            static function(ExitCode $exitCode, string $action) use ($processes): ExitCode {
+                if (!$exitCode->isSuccessful()) {
+                    return $exitCode;
+                }
+
+                return $processes
+                    ->execute(ServerCommand::foreground($action))
+                    ->wait()
+                    ->exitCode();
+            }
+        );
+        $env->exit($exitCode->toInt());
+    }
+
+    public function __toString(): string
+    {
+        return <<<USAGE
+install
+
+This will install rabbitmq on the machine
+
+It only works for Debian Stretch as we need to configure the repositories
+from which we'll fetch rabbitmq package
+USAGE;
+    }
+}
---
+++ tests/Command/InstallTest.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\InfrastructureAMQP\Command;
+
+use Innmind\InfrastructureAMQP\Command\Install;
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Server\Control\{
+    Server,
+    Server\Processes,
+    Server\Process,
+    Server\Process\ExitCode,
+};
+use PHPUnit\Framework\TestCase;
+
+class InstallTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new Install($this->createMock(Server::class))
+        );
+    }
+
+    public function testInvokation()
+    {
+        $install = new Install(
+            $server = $this->createMock(Server::class)
+        );
+        $server
+            ->expects($this->once())
+            ->method('processes')
+            ->willReturn($processes = $this->createMock(Processes::class));
+        $processes
+            ->expects($this->exactly(12))
+            ->method('execute')
+            ->withConsecutive(
+                ['echo "deb https://dl.bintray.com/rabbitmq/debian stretch main" | tee /etc/apt/sources.list.d/bintray.rabbitmq.list'],
+                ['wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | apt-key add -'],
+                ['apt-get update'],
+                ['apt-get install libsctp1 -y'],
+                ['wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_20.3-1~debian~stretch_amd64.deb'],
+                ['dpkg -i esl-erlang_20.3-1~debian~stretch_amd64.deb'],
+                ['rm esl-erlang_20.3-1~debian~stretch_amd64.deb'],
+                ['apt-get install rabbitmq-server -y'],
+                ['rabbitmq-plugins enable rabbitmq_management'],
+                ['wget http://localhost:15672/cli/rabbitmqadmin'],
+                ['chmod +x rabbitmqadmin'],
+                ['mv rabbitmqadmin /usr/local/bin/rabbitmqadmin']
+            )
+            ->willReturn($process = $this->createMock(Process::class));
+        $process
+            ->expects($this->exactly(12))
+            ->method('wait')
+            ->will($this->returnSelf());
+        $process
+            ->expects($this->exactly(12))
+            ->method('exitCode')
+            ->willReturn(new ExitCode(0));
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->once())
+            ->method('exit')
+            ->with(0);
+
+        $this->assertNull($install(
+            $env,
+            new Arguments,
+            new Options
+        ));
+    }
+
+    public function testUsage()
+    {
+        $expected = <<<USAGE
+install
+
+This will install rabbitmq on the machine
+
+It only works for Debian Stretch as we need to configure the repositories
+from which we'll fetch rabbitmq package
+USAGE;
+
+        $this->assertSame($expected, (string) new Install($this->createMock(Server::class)));
+    }
+}

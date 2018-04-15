33e18dca121b9e6f672cf49247233c9402707edc
Baptiste Langlade <baptouuuu@gmail.com>
2018-04-15 15:10:01 +0200

    add command to setup users

---
+++ src/Command/SetupUsers.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Infrastructure\AMQP\Command;
+
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\RabbitMQ\Management\Control;
+
+final class SetupUsers implements Command
+{
+    private $rabbitmq;
+
+    public function __construct(Control $rabbitmq)
+    {
+        $this->rabbitmq = $rabbitmq;
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        $this->rabbitmq->users()->declare('monitor', \random_bytes(32), 'administrator');
+        $this->rabbitmq->users()->declare('consumer', \random_bytes(32));
+        $this->rabbitmq->permissions()->declare('/', 'monitor', '.*', '.*', '.*');
+        $this->rabbitmq->permissions()->declare('/', 'consumer', '.*', '.*', '.*');
+        $this->rabbitmq->users()->delete('guest');
+    }
+
+    public function __toString(): string
+    {
+        return <<<USAGE
+setup-users
+
+This will create a user from administration and another one for clients
+USAGE;
+    }
+}

5e5cf7cb4d1e3378e082c92ae764f3a5589c15ae
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-02 16:35:57 +0200

    replace service container by a bootstrap function

---
+++ src/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Infrastructure\AMQP;
+
+use Innmind\Infrastructure\AMQP\{
+    Command\Install,
+    Command\SetupUsers,
+};
+use Innmind\CLI\Commands;
+use Innmind\Server\Control\ServerFactory;
+use Innmind\RabbitMQ\Management\Control\Control;
+
+function bootstrap(): Commands
+{
+    $server = ServerFactory::build();
+
+    return new Commands(
+        new Install($server),
+        new SetupUsers(
+            new Control($server)
+        )
+    );
+}
